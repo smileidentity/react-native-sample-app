121583da432883f64b79c066fe5f757ccdc03788
Arlon Mukeba <arlon.mukeba@gmail.com>
2022-02-22 09:45:56 +0200

    Updated pods

---
+++ ios/RnSmileId-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  RnSmileId-Bridging-Header.h
+//  rn_example
+//
+//  Created by Arlon Mukeba on 2022/02/21.
+//
+
+#ifndef RnSmileId_Bridging_Header_h
+#define RnSmileId_Bridging_Header_h
+
+
+#endif /* RnSmileId_Bridging_Header_h */
---
+++ ios/RnSmileId.m
@@ -0,0 +1,8 @@
+//
+//  RnSmileId.m
+//  rn_example
+//
+//  Created by Arlon Mukeba on 2022/02/21.
+//
+
+#import <Foundation/Foundation.h>
